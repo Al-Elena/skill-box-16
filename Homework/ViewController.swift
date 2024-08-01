031f23042f4c80a0392c463c9cce5063e011ac84
Al-Elena <al_elena@tut.by>
2024-08-01 21:17:22 +0300

    First commit

---
+++ Homework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework
+//
+//  Created by Елена Алексейчикова on 1.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
